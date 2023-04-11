8035ef1dbc7acb8529e198d549bc32d85c8158d1
Marvin Martinez <marvin.s.martinez.h@gmail.com>
2023-04-11 23:18:25 +0000

    Generador linea pedido

---
+++ api_sig/generador_datos.py
@@ -0,0 +1,13 @@
+#import django.apps
+#os.environ.setdefault ("DJANGO_SETTINGS_MODULE", "sigtl.settings")
+#django.setup()
+#from models import Producto
+#from api_sig.models import Sucursal
+#from api_sig.models import Pedido
+#from api_sig.models import LineaPedido
+
+#producto = Producto.objects.get(1)
+#sucursal = Sucursal.objects.get(1)
+#pedido = Pedido.objects.create(1, '2022-02-14', 'Vendido')
+print("hola")
+#lineaPedido = LineaPedido.objects.create()
