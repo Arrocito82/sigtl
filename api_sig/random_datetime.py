e6c0610c372bbedf603ae86ff7ec0ca9f8b9611b
Andrea Monterrossa <moralesandrea.edr4@gmail.com>
2023-04-11 23:04:02 +0000

    generador de datetime con horas pico aleatorio

--- api_sig/random_datetime.py
+++ api_sig/random_datetime.py
@@ -3,25 +3,37 @@ import random
 # class RandomDateTime(object):
 start_date = datetime.datetime(2022, 1, 4, 8) 
 # Establece la fecha inicial a las 8 am del 3 de enero de 2022
-end_date = datetime.datetime(2022, 1, 4, 16) 
+end_date = datetime.datetime(2022, 1, 4, 19) 
 # Establece la fecha final a las 4 pm del 3 de enero de 2022
 current_date = start_date # Inicializa la fecha actual con la fecha inicial
-# Rango de tiempo aleatorio preferido
-MIN_MINUTOS=0
-MAX_MINUTOS=3
-MIN_MINUTOS_HORA_PICO=0
-MAX_MINUTOS_HORA_PICO=1
+# RANGO DE SEGUNDOS ALEATORIOS
 MIN_SEG=0
 MAX_SEG=59
+# MINUTOS ENTRE VENTAS
+MIN_MINUTOS=0
+MAX_MINUTOS=3
+# HORAS MINUTOS ENTRE VENTAS EN HORA PICO
+MIN_MINUTOS_HORA_PICO=0
+MAX_MINUTOS_HORA_PICO=1
+# PERIODO DE HORAS PICO
+HORA_PICO_MIN_MANANA=11
+HORA_PICO_MAX_MANANA=13
+HORA_PICO_MIN_TARDE=17
+HORA_PICO_MAX_TARDE=19
 # HORA PICO 11-1, 5-7 
 
 def get_current_time():
-    minutos_aleatorio= random.randint(MIN_MINUTOS, MAX_MINUTOS) 
-    segundos_aleatorio= random.randint(MIN_SEG, MAX_SEG) 
-    # Genera un número aleatorio entre 15 y 95 (ambos incluidos)
-    delta = datetime.timedelta(minutes=minutos_aleatorio,seconds=segundos_aleatorio) 
     global current_date, start_date,end_date
-    # Establece un intervalo de tiempo de 15 minutos
+    
+    segundos_aleatorio= random.randint(MIN_SEG, MAX_SEG)
+    # verificando si es hora pico
+    # print(type(current_date.hour))
+    horas=current_date.hour
+    if horas>HORA_PICO_MIN_MANANA and horas<HORA_PICO_MAX_MANANA or horas>HORA_PICO_MIN_TARDE and horas<HORA_PICO_MAX_TARDE:
+        minutos_aleatorio= random.randint(MIN_MINUTOS_HORA_PICO, MAX_MINUTOS_HORA_PICO) 
+    else:
+        minutos_aleatorio= random.randint(MIN_MINUTOS, MAX_MINUTOS) 
+    delta = datetime.timedelta(minutes=minutos_aleatorio,seconds=segundos_aleatorio) 
     current_date += delta # Añade el intervalo de tiempo a la fecha actual para obtener la siguiente fecha
     if current_date >= end_date: # Mientras la fecha actual sea menor que la fecha final
         current_date = start_date + datetime.timedelta(days=1)
--- api_sig/test.py
+++ api_sig/test.py
@@ -1,4 +1,4 @@
 import random_datetime
 
-for n in range(50):
-    print(random_datetime.get_current_time().strftime("%Y-%m-%d %H:%M:%S.%f+00"))
+for n in range(200):
+    print(random_datetime.get_current_time().strftime("n. "+str(n)+" %Y-%m-%d %H:%M:%S.%f+00"))
