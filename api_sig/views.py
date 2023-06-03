a840116386a92d91822059217f30592cdef786f5
MarvinSMartinez <marvin.s.martinez.h@gmail.com>
2023-06-03 00:20:34 +0000

    productos Danados y guardar Movimientos

--- api_sig/views.py
+++ api_sig/views.py
@@ -21,8 +21,9 @@ def index(request):
         'Msg':mensaje[index]
         })
 
+#VALIDAR EXCEL MOVIMIENTOS
 @csrf_exempt
-def create_db(request):
+def validarMovimientos(request):
     data=request.body
     #print(data)
     string_data=request.body.decode('utf-8')
@@ -68,3 +69,134 @@ def create_db(request):
             # print(e)
     # print(data_dict)
     return JsonResponse(data_dict, safe=False)
+
+#GUARDAR EXCEL MOVIMIENTOS EN DB
+@csrf_exempt
+def crearMovimientos(request):
+    data=request.body
+    #print(data)
+    string_data=request.body.decode('utf-8')
+    data_dict = json.loads(string_data)
+    error=False
+
+    contador = 0
+
+    formato_fecha = "%Y-%m-%d %H:%M:%S %z"
+
+    fecha=datetime.now()
+    
+    p=parser()
+
+    for l in data_dict:
+
+        movimiento=Movimiento()
+        movimiento.id_movimiento=int(l["id_movimiento"])
+        movimiento.id_sucursal_id=int(l["id_sucursal_id"])
+        movimiento.id_producto_id=int(l["id_producto_id"])
+
+        # print(l["fecha_registro"])
+        fecha_div=l["fecha_registro"].split("+")
+        # print(fecha_div[0])
+        fecha=datetime.strptime(fecha_div[0], "%Y-%m-%d %H:%M:%S")
+        # print(type(fecha_div))
+        #my_datetime_utc = fecha.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+        #print("hola", my_datetime_utc)
+        movimiento.fecha_registro=fecha
+
+        movimiento.valorUnitario=float(l["valorUnitario"])
+        movimiento.detalle=l["detalle"]
+        movimiento.cantidad=int(l["cantidad"])
+        movimiento.total=float(l["total"])
+        movimiento.tipo =l["tipo"].strip(" ")
+        try:
+            movimiento.full_clean()
+            l["valido"]=True
+            l["errores"]={}
+            movimiento.save()
+        except ValidationError as e:
+            l["valido"]=False
+            l["errores"]=e.message_dict
+            # print(e)
+    # print(data_dict)
+    return JsonResponse(data_dict, safe=False)
+
+#VALIDAR EXCEL PRODUCTOS DAÑADOS
+@csrf_exempt
+def validarProductosDanados(request):
+
+    string_data=request.body.decode('utf-8')
+    data_dict = json.loads(string_data)
+
+    fecha=datetime.now()
+    
+    for l in data_dict:
+
+        productoDanado=ProductoDanado()
+        productoDanado.id_productoDanado=int(l["id_productoDanado"])
+        productoDanado.id_producto_id=int(l["id_producto_id"])
+
+        # print(l["fecha_registro"])
+        fecha_div=l["fecha_registro"].split("+")
+        # print(fecha_div[0])
+        fecha=datetime.strptime(fecha_div[0], "%Y-%m-%d %H:%M:%S")
+        # print(type(fecha_div))
+        #my_datetime_utc = fecha.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+        #print("hola", my_datetime_utc)
+        productoDanado.fecha_registro=fecha
+
+        productoDanado.detalle=l["detalle"]
+        productoDanado.cantidad=int(l["cantidad"])
+
+        try:
+            productoDanado.full_clean()
+            l["valido"]=True
+            l["errores"]={}
+        except ValidationError as e:
+            l["valido"]=False
+            l["errores"]=e.message_dict
+            # print(e)
+    # print(data_dict)
+    return JsonResponse(data_dict, safe=False)
+
+#VALIDAR EXCEL PRODUCTOS DAÑADOS
+@csrf_exempt
+def crearProductosDanados(request):
+
+    string_data=request.body.decode('utf-8')
+    data_dict = json.loads(string_data)
+
+    fecha=datetime.now()
+    
+    for l in data_dict:
+
+        productoDanado=ProductoDanado()
+        productoDanado.id_productoDanado=int(l["id_productoDanado"])
+        productoDanado.id_producto_id=int(l["id_producto_id"])
+
+        # print(l["fecha_registro"])
+        fecha_div=l["fecha_registro"].split("+")
+        # print(fecha_div[0])
+        fecha=datetime.strptime(fecha_div[0], "%Y-%m-%d %H:%M:%S")
+        # print(type(fecha_div))
+        #my_datetime_utc = fecha.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+        #print("hola", my_datetime_utc)
+        productoDanado.fecha_registro=fecha
+
+        productoDanado.detalle=l["detalle"]
+        productoDanado.cantidad=int(l["cantidad"])
+
+        try:
+            productoDanado.full_clean()
+            l["valido"]=True
+            l["errores"]={}
+            productoDanado.save()
+        except ValidationError as e:
+            l["valido"]=False
+            l["errores"]=e.message_dict
+            # print(e)
+    # print(data_dict)
+    return JsonResponse(data_dict, safe=False)
+@csrf_exempt
+def reporteIngresosCostos(request):
+    
+    return
