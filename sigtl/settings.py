7792b7b8321e6d069f827dd729df25231b3ebe46
Andrea Monterrossa <moralesandrea.edr4@gmail.com>
2023-06-10 19:21:55 +0000

    reparación de error installed apps

--- sigtl/settings.py
+++ sigtl/settings.py
@@ -75,7 +75,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'rest_framework'
+    'rest_framework',
+    'django_postgres_createdb'
 ]
 
 MIDDLEWARE = [
